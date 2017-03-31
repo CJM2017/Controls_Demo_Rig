4d921965cab4edf70b098bf12cbfbfdfa3007f8c
Connor McCann <cjmccann@bu.edu>
2017-03-30 21:15:35 -0400

    initial

--- Arm_Controller/Arm_Controller.ino
+++ Arm_Controller/Arm_Controller.ino
@@ -23,10 +23,33 @@
 #include <Wire.h>
 
 
+#define PIN_INPUT 0
+#define PIN_OUTPUT 3
+
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+
+//Specify the links and initial tuning parameters
+double Kp=2, Ki=5, Kd=1;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
 void setup() {
+
+  // Setting up the PID Controller
+  Input = analogRead(PIN_INPUT);
+  Setpoint = 100;
+
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
  
 }
 
 void loop() {
 
+  //
+  Input = analogRead(PIN_INPUT);
+  myPID.Compute();
+  analogWrite(PIN_OUTPUT, Output);
+
 }
+
