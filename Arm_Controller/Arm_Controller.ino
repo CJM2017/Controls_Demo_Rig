a1c606b33e29b7789b911bc22ce58d1f58cc3d2a
Connor McCann <cjmccann@bu.edu>
2017-03-30 21:59:37 -0400

    progress

--- Arm_Controller/Arm_Controller.ino
+++ Arm_Controller/Arm_Controller.ino
@@ -24,24 +24,28 @@
 
 #define PID_INPUT   0   // read the value from the IMU 
 #define PID_OUTPUT  3   // set the value form the ESC
-#define ESC_PIN     10  // Where the ESC is connected
+#define ESC_PIN     10  // Where the ESC is connected (PWM)
 
 
 Servo ESC;  // Create the ESC Servo
  
-double Setpoint;
-double Input, Output; // input is form IMU & output is going to the ESC
+double setpoint;
+double Input;   // input is form IMU
+double Output;  // output is going to the ESC
 
-double Kp=1, Ki=0, Kd=0;
-PID motor_PID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT); // set the characteristics of the controller
+double Kp=1, Ki=0 , Kd=0;
+PID motor_PID(&Input, &Output, &setpoint, Kp, Ki, Kd, DIRECT); // set the characteristics of the controller
 
 void setup() {
 
+  // Serial command line interface
+  Serial.begin(9600);
+  
   // Setting up the ESC
   ESC.attach(ESC_PIN);
   
   // Setting up the PID Controller
-  Setpoint;   // set this for the angle you want
+  setpoint;   // set this for the angle you want
 
   //turn the PID on
   motor_PID.SetMode(AUTOMATIC);
@@ -50,6 +54,19 @@ void setup() {
 
 void loop() {
 
+  // Get the setpoint value from user input
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    setpoint = Serial.read();
+  
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(setpoint, DEC);
+  }
+  else {
+    setpoint = 0; // default value
+  }
+  
   // Execute PID Controller
   motor_PID.Compute();
   ESC.write(Output);
